a8444c53cd84d8199db0b112675a904140664f4e
syoake <ryupark1006@gmail.com>
2022-08-14 22:40:50 +0900

    소셜 로그인 기능 추가

---
+++ src/components/Router.js
@@ -0,0 +1,20 @@
+import { HashRouter, Route, Routes } from "react-router-dom";
+import Auth from "routes/Auth";
+import Home from "routes/Home";
+
+const AppRouter = ({ isLoggedIn }) => {
+    return (
+        <HashRouter>
+            <Routes>
+                {isLoggedIn ? (
+                    <Route path="/" element={ <Home /> } />
+                ) : (
+                    <Route path="/" element={ <Auth /> } />
+
+                )}
+            </Routes>
+        </HashRouter>
+    );
+};
+
+export default AppRouter;
