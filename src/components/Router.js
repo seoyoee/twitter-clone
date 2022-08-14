31a408cf97634cc301ba96f19cdfae1a73d495b4
syoake <ryupark1006@gmail.com>
2022-08-14 23:20:34 +0900

    내비게이션 추가

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,15 +1,21 @@
 import { HashRouter, Route, Routes } from "react-router-dom";
 import Auth from "routes/Auth";
 import Home from "routes/Home";
+import Profile from "routes/Profile";
+import Navigation from "./Navigation";
 
 const AppRouter = ({ isLoggedIn }) => {
     return (
         <HashRouter>
+            {isLoggedIn && <Navigation />}
             <Routes>
                 {isLoggedIn ? (
-                    <Route path="/" element={ <Home /> } />
+                    <>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/profile" element={<Profile />} />
+                    </>
                 ) : (
-                    <Route path="/" element={ <Auth /> } />
+                    <Route path="/" element={<Auth />} />
 
                 )}
             </Routes>
