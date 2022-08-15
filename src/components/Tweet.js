23b409aae7f680389c45064d81763e4069447c8d
syoake <ryupark1006@gmail.com>
2022-08-15 15:15:08 +0900

    트윗 수정 기능 추가

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -1,20 +1,51 @@
 import { dbService } from "fbase";
+import { useState } from "react";
 
 const Tweet = ({ tweetObj, isOwner }) => {
+    const [editing, setEditing] = useState(false);
+    const [newTweet, setNewTweet] = useState(tweetObj.text);
+
     const onDeleteClick = async () => {
         const ok = window.confirm("삭제하시겠습니까?");
         if (ok) {
-            const data = await dbService.doc(`tweets/${tweetObj.id}`).delete();
+            await dbService.doc(`tweets/${tweetObj.id}`).delete();
         }
+    };
+
+    const toggleEditing = () => setEditing((prev) => !prev);
+
+    const onChange = (event) => {
+        const {
+            target: { value },
+        } = event;
+        setNewTweet(value);
+    };
+
+    const onSubmit = async (event) => {
+        event.preventDefault();
+        await dbService.doc(`tweets/${tweetObj.id}`).update({ text: newTweet });
+        setEditing(false);
     };
 
     return (
         <div>
-            <h4>{tweetObj.text}</h4>
-            {isOwner && (
+            {editing ? (
                 <>
-                    <button onClick={onDeleteClick}>Delete Tweet</button>
-                    <button>Edit Tweet</button>
+                    <form onSubmit={onSubmit}>
+                        <input onChange={onChange} value={newTweet} required />
+                        <input type="submit" value="Update Tweet" />
+                    </form>
+                    <button onClick={toggleEditing}>Cancel</button>
+                </>
+            ) : (
+                <>
+                    <h4>{tweetObj.text}</h4>
+                    {isOwner && (
+                        <>
+                            <button onClick={onDeleteClick}>Delete Tweet</button>
+                            <button onClick={toggleEditing}>Edit Tweet</button>
+                        </>
+                    )}
                 </>
             )}
         </div>
